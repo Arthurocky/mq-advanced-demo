45624937f0cd448e7117b478d040bc09c893d3eb
Athurocky <arthurocky@user.noreply.gitee.com>
2023-02-20 22:18:11 +0800

    Test Exchange Absence

--- publisher/src/main/java/cn/itcast/mq/PublisherApplication.java
+++ publisher/src/main/java/cn/itcast/mq/PublisherApplication.java
@@ -1,11 +1,32 @@
 package cn.itcast.mq;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
+@Slf4j
 @SpringBootApplication
 public class PublisherApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(PublisherApplication.class);
     }
+
+    /**
+     * 消息到交换机，但是没有到达队列则会调用ReturnCallback方法
+     *
+     * @param rabbitTemplate
+     */
+    @Resource
+    public void setReturnCallback(RabbitTemplate rabbitTemplate)
+    {
+        rabbitTemplate.setReturnCallback(
+                (message, replyCode, replyText, exchange, routingKey) ->
+                        log.info("消息发送失败，应答码{}，原因{}，交换机{}，路由键{},消息{}"
+                                , replyCode, replyText, exchange, routingKey, message.toString()));
+    }
+
 }
--- publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
+++ publisher/src/test/java/cn/itcast/mq/spring/SpringAmqpTest.java
@@ -3,10 +3,16 @@ package cn.itcast.mq.spring;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.concurrent.FailureCallback;
+import org.springframework.util.concurrent.SuccessCallback;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -16,9 +22,43 @@ public class SpringAmqpTest {
     private RabbitTemplate rabbitTemplate;
 
     @Test
-    public void testSendMessage2SimpleQueue() throws InterruptedException {
+    public void testSendMessagePublishConfirm() throws InterruptedException
+    {
         String routingKey = "simple";
         String message = "hello, spring amqp!";
-        rabbitTemplate.convertAndSend("camq.topic", routingKey, message);
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+        correlationData.getFuture().addCallback(new SuccessCallback<CorrelationData.Confirm>() {
+            @Override
+            public void onSuccess(CorrelationData.Confirm confirm)
+            {
+                if (confirm.isAck()) {
+                    // 返回ack
+                    //log.info("消息发送成功!");
+                    System.out.println("消息发送成功!");
+                } else {
+                    // 返回nack
+                    log.error("消息发送失败! {}", confirm.getReason());
+                    System.out.println("消息发送失败! " + confirm.getReason());
+                }
+            }
+        }, new FailureCallback() {
+            @Override
+            public void onFailure(Throwable throwable)
+            {
+                // 异常。网络超时...
+                log.error("消息发送异常了", throwable);
+                // 发短信通知
+                // 重试
+            }
+        });
+        //设置交换机不存在
+        rabbitTemplate.convertAndSend("camq.direct", routingKey, message, correlationData);
+
+        try {
+            Thread.sleep(10000);
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
